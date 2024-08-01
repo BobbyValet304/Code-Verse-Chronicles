c516d234783daa0c6a3e0d4330b005c84cf94b28
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-08-01 16:11:19 -0400

    gives models for comments, posts, and users

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Post extends Model {
+ 
+  }
+
+Post.init({
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.STRING
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  }
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'post'
+});
+
+module.exports = Post;
